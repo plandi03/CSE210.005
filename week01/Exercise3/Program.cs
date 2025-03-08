e619e4fceeb47e7c5a988bf1aaf729a8fb8d62a9
Your Name <you@example.com>
2025-03-08 15:13:07 -0400

    Finish exercise number 3, gussing game

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -4,6 +4,33 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
+        string response = "";
+        Console.Write("Do you want to play a game? (Yes/No): ");
+        response = Console.ReadLine();
+        do
+        {
+            Random randomGenerator = new Random();
+            int magicNumber = randomGenerator.Next(1, 101);
+            int userGuess = 0;
+            int numberOfGuesses = 0;
+            do{
+                Console.Write("Enter your guess: ");
+                userGuess = int.Parse(Console.ReadLine());
+                numberOfGuesses++;
+                if(userGuess < magicNumber){
+                    Console.WriteLine("Too low!");
+                }else if(userGuess > magicNumber){
+                    Console.WriteLine("Too high!");
+                }
+                if(userGuess == magicNumber){
+                    Console.WriteLine("Congratulations! You guessed the magic number in " + numberOfGuesses + " guesses!");
+                }
+            }while(userGuess != magicNumber);
+        Console.Write("Do you want to play a game? (Yes/No): ");
+            response = Console.ReadLine();
+
+        }
+        while(response == "Yes");
+        
     }
 }
